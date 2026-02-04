585ea98d20ece0afdf8ed9e0a669824a5afb2476
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-02-04 15:45:16 +0000

    Change grid['RVf'] to cell['RVf'] in Q calculation

--- src/monarchs/physics/surface_fluxes.py
+++ src/monarchs/physics/surface_fluxes.py
@@ -69,9 +69,9 @@ def sfc_flux(
     epsilon_ice = 0.98
     epsilon_rock = 0.92 # (from Minco and Pappalardo, 2021)
     sigma = 5.670374e-8
-    if grid['RVf'] = 0:
+    if cell['RVf'] = 0:
         Q = epsilon * LW_in + (1 - alpha) * SW_in + Flat + Fsens
-    elif grid['RVf'] < 1:
+    elif cell['RVf'] < 1:
         Q = ((1 - cell['RVf']) * epsilon_ice * LW_in) + ((1 - alpha) * (1 - cell['RVf']) * SW_in) + F_lat + F_sens + (epsilon_rock * T_rock**4 * sigma * cell['RVf'] * LW_in)
     else:
         # Don't calculate Q, cell is invalid - how?
