1a14a46ddfbf077eb555a8f38d7bd635594159ce
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-02-13 16:16:37 +0000

    Update calc_mass_cell() with consideration for RVf

--- src/monarchs/core/utils.py
+++ src/monarchs/core/utils.py
@@ -209,17 +209,31 @@ def calc_mass_sum(cell):
     total_mass : float
         Amount of mass in the system, in arbitrary units.
     """
-    total_mass = (
-        np.sum(
-            cell["Sfrac"] * cell["rho_ice"] * (cell["firn_depth"] / cell["vert_grid"])
+    if cell["RVf"] = 0:
+        total_mass = (
+            np.sum(
+                cell["Sfrac"] * cell["rho_ice"] * (cell["firn_depth"] / cell["vert_grid"])
+            )
+            + np.sum(
+                cell["Lfrac"] * cell["rho_water"] * (cell["firn_depth"] / cell["vert_grid"])
+            )
+            + cell["lake_depth"] * cell["rho_water"]
+            + cell["lid_depth"] * cell["rho_ice"]
+            + cell["v_lid_depth"] * cell["rho_ice"]
         )
-        + np.sum(
-            cell["Lfrac"] * cell["rho_water"] * (cell["firn_depth"] / cell["vert_grid"])
+    else cell["RVf"] < 1:
+        total_mass = (
+            (np.sum(
+                cell["Sfrac"] * cell["rho_ice"] * (cell["firn_depth"] / cell["vert_grid"])
+            )
+            + np.sum(
+                cell["Lfrac"] * cell["rho_water"] * (cell["firn_depth"] / cell["vert_grid"])
+            )
+            + cell["lake_depth"] * cell["rho_water"]
+            + cell["lid_depth"] * cell["rho_ice"]
+            + cell["v_lid_depth"] * cell["rho_ice"])
+        * (1 - cell["RVf"])
         )
-        + cell["lake_depth"] * cell["rho_water"]
-        + cell["lid_depth"] * cell["rho_ice"]
-        + cell["v_lid_depth"] * cell["rho_ice"]
-    )
     return total_mass
 
 
