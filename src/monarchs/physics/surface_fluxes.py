01e0ead2f73bd10020dd30c7fee6823487777a02
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-02-24 14:50:47 +0000

    Update Q calculation with exponent

--- src/monarchs/physics/surface_fluxes.py
+++ src/monarchs/physics/surface_fluxes.py
@@ -71,7 +71,7 @@ def sfc_flux(
     if cell['RVf'] = 0:
         Q = epsilon_ice * LW_in + (1 - alpha) * SW_in + Flat + Fsens
     else cell['RVf'] < 1:
-        Q = epsilon_ice * LW_in + (1 - alpha) * SW_in + F_lat + F_sens + (epsilon_rock * T_rock**4 * sigma * cell['RVf'] * LW_in)
+        Q = epsilon_ice * LW_in + (1 - alpha) * SW_in + F_lat + F_sens + ((epsilon_rock * T_rock**4 * sigma * cell['RVf'] * LW_in) ** 0.5)
     return Q
 
 
