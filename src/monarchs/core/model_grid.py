ebb86b9cbd163b6fe0101b877985e52911e477f1
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-02-27 16:31:13 +0000

    Update load_RVf function call

--- src/monarchs/core/model_grid.py
+++ src/monarchs/core/model_grid.py
@@ -77,7 +77,7 @@ def initialise_iceshelf(
     iceshelf["rho_lid"] = 917.0 * np.ones((num_rows, num_cols, vert_grid_lid))
     iceshelf["firn_temperature"] = firn_temperature
     if hasattr(model_setup, "RVf_input_filepath"):
-        iceshelf["RVf"] = load_RVf(model_setup, model_setup.RVf_input_filepath) # Calling load_RVf function
+        iceshelf["RVf"] = load_RVf(model_setup) # Calling load_RVf function
     else:
         iceshelf["RVf"] = np.zeros((num_rows, num_cols, vert_grid))
     if np.isnan(Sfrac).all():
