87a4dfae49bfab916c2e416e97df3d285d4d28b6
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-03-03 11:18:22 +0000

    Update import_T_rock.py

--- src/monarchs/rock_view/import_T_rock.py
+++ src/monarchs/rock_view/import_T_rock.py
@@ -30,8 +30,7 @@ def load_T_rock(model_setup):
      
      # Checking for the existence of the file and that it can be read.
      if os.access(model_setup.T_rock_input_filepath, os.R_OK):
-         T_rock_data = pd.read_csv(model_setup.T_rock_input_filepath, delimiter=',', header='infer',
-                                   parse_dates=['valid_time'], date_format='%Y-%m-%mT%hh:%mm:%ss') # Change column header and datetime format if required.
+         T_rock_data = pd.read_csv(model_setup.T_rock_input_filepath, delimiter=',', header='infer', parse_dates=['valid_time']) # Change column header if required.
      else:
          raise IOError(f"The file {model_setup.T_rock_input_filepath} is not readable.")
     
