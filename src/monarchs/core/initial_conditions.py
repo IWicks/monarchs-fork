fff479755af3dfa3dab681d2d6953ac86dee9c8b
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-03-04 10:29:26 +0000

    Add model_setup parameter to initialise_iceshelf and create_model_grid

--- src/monarchs/core/initial_conditions.py
+++ src/monarchs/core/initial_conditions.py
@@ -194,6 +194,7 @@ def rho_init_emp(z, rho_sfc, z_t):
 
 
 def create_model_grid(
+        model_setup,
         row_amount,
         col_amount,
         firn_depth,
@@ -243,6 +244,7 @@ def create_model_grid(
     )
     dtype = get_spec(vert_grid, vert_grid_lake, vert_grid_lid)
     grid = initialise_iceshelf(
+        model_setup,
         row_amount,
         col_amount,
         vert_grid,
