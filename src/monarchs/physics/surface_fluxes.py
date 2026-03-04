de16d36afa57b9ba1ff16ba55b96c73b9fbff892
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-03-04 11:59:14 +0000

    Update surface_fluxes.py cell['RVf'] syntax

--- src/monarchs/physics/surface_fluxes.py
+++ src/monarchs/physics/surface_fluxes.py
@@ -68,7 +68,7 @@ def sfc_flux(
     sigma = 5.670374e-8
     if cell['RVf'] == 0:
         Q = epsilon_ice * LW_in + (1 - alpha) * SW_in + Flat + Fsens
-    else cell['RVf'] < 1:
+    else:
         Q = epsilon_ice * LW_in + (1 - alpha) * SW_in + F_lat + F_sens + ((epsilon_rock * T_rock**4 * sigma * cell['RVf'] * LW_in) ** 0.5)
     return Q
 
