6182c575a61f6175dc85827d9baa044638f69c64
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-05-05 11:10:25 +0100

    Remote print test

--- src/monarchs/physics/lid_functions.py
+++ src/monarchs/physics/lid_functions.py
@@ -149,7 +149,7 @@ def virtual_lid(cell, dt, LW_in, SW_in, T_air, p_air, T_dp, T_rock, wind):
         cell["lid"] = False
         cell["v_lid"] = False
     new_mass = utils.calc_mass_sum(cell)
-    print("Lid new mass - original mass =", new_mass - original_mass)
+    print("v lid temp = ", cell["virtual_lid_temperature"], "Q = ", Q, "v lid depth = ", cell["v_lid_depth"], "lake depth = ", cell["lake_depth"])
     try:
         assert abs(new_mass - original_mass) < 1.5 * 10**-7
     except Exception:
