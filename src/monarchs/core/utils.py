4ca2186f7cb6a391c9555f30fac7e15d98ca9517
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-05-05 12:10:33 +0100

    Update calc_mass_sum(cell) with RVf consideration

--- src/monarchs/core/utils.py
+++ src/monarchs/core/utils.py
@@ -209,7 +209,7 @@ def calc_mass_sum(cell):
     total_mass : float
         Amount of mass in the system, in arbitrary units.
     """
-    total_mass = (
+    total_mass = ((
         np.sum(
             cell["Sfrac"] * cell["rho_ice"] * (cell["firn_depth"] / cell["vert_grid"])
         )
@@ -219,6 +219,7 @@ def calc_mass_sum(cell):
         + cell["lake_depth"] * cell["rho_water"]
         + cell["lid_depth"] * cell["rho_ice"]
         + cell["v_lid_depth"] * cell["rho_ice"]
+        )
         * (1 - cell["RVf"])
     )
     return total_mass
