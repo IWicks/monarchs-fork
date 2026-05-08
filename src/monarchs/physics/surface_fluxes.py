a81de5c74d471df282f4f32a5f7fd70e27a4ec29
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-05-08 12:09:46 +0100

    Remote testing

--- src/monarchs/physics/surface_fluxes.py
+++ src/monarchs/physics/surface_fluxes.py
@@ -164,11 +164,10 @@ def bulk_fluxes(wind, T_air, T_sfc, p_air, T_dp):
     a4 = 32.19
     e_sat = a1 * np.exp(a3 * (T_dp - T_0) / (T_dp - a4))
     s_hum = R_dry / R_sat * e_sat / (p_air - e_sat * (1 - R_dry / R_sat))
-    for x in wind:
-        if x == 0:
-            Ri = 0
-        else:
-            Ri = g * (T_air - T_sfc) * dz / (T_air * wind**2)
+    if wind == 0:
+        Ri = 0
+    else:
+        Ri = g * (T_air - T_sfc) * dz / (T_air * wind**2)
     if Ri < 0:
         CT = CT0 * (1 - 2 * b * Ri / (1 + c * abs(Ri) ** 0.5))
     else:
