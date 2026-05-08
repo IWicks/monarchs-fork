e67f02917c72cf431b80256edebf8936ec111e17
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-05-08 11:48:00 +0100

    Wind indexing test

--- src/monarchs/physics/surface_fluxes.py
+++ src/monarchs/physics/surface_fluxes.py
@@ -164,10 +164,11 @@ def bulk_fluxes(wind, T_air, T_sfc, p_air, T_dp):
     a4 = 32.19
     e_sat = a1 * np.exp(a3 * (T_dp - T_0) / (T_dp - a4))
     s_hum = R_dry / R_sat * e_sat / (p_air - e_sat * (1 - R_dry / R_sat))
-    if wind == 0:
-        Ri = 0
-    else:
-        Ri = g * (T_air - T_sfc) * dz / (T_air * wind**2)
+    for x in wind:
+        if x == 0:
+            Ri = 0
+        else:
+            Ri = g * (T_air - T_sfc) * dz / (T_air * wind**2)
     if Ri < 0:
         CT = CT0 * (1 - 2 * b * Ri / (1 + c * abs(Ri) ** 0.5))
     else:
