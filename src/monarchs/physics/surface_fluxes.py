ed1fe69639608f140afa6860894362a238dd6152
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-06-09 15:07:41 +0100

    Tab fixing

--- src/monarchs/physics/surface_fluxes.py
+++ src/monarchs/physics/surface_fluxes.py
@@ -163,24 +163,19 @@ def bulk_fluxes(wind, T_air, T_sfc, p_air, T_dp):
     T_0 = 273.16
     a3 = 17.502
     a4 = 32.19
-    
     p_air *= 100  # convert hPa to Pa for consistency
     e_sat = a1 * np.exp(a3 * (T_dp - T_0) / (T_dp - a4))
     s_hum = R_dry / R_sat * e_sat / (p_air - e_sat * (1 - R_dry / R_sat))
-	
 	# checking value type of wind
 	print(wind, np.isscalar(wind))
-    
 	if wind == 0:
 		Ri = 0
 	else:
 		Ri = g * (T_air - T_sfc) * dz / (T_air * wind**2)
-        
 	if Ri < 0:
 		CT = CT0 * (1 - 2 * b * Ri / (1 + c * abs(Ri) ** 0.5))
 	else:
 		CT = CT0 * (1 + b * Ri) **-2
-		
     L = 2.501 * 10**6
     p_v = 2.53 * 10**8 * np.exp(-5420 / T_sfc)
     q_0 = 0.622 * p_v / (p_air - 0.378 * p_v)
