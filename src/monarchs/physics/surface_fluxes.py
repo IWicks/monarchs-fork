9a87c1b8555ae04a93b94f09e9cf617d2754b93e
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-06-10 10:49:50 +0100

    Debug/testing changes

--- src/monarchs/physics/surface_fluxes.py
+++ src/monarchs/physics/surface_fluxes.py
@@ -166,9 +166,6 @@ def bulk_fluxes(wind, T_air, T_sfc, p_air, T_dp):
     p_air *= 100  # convert hPa to Pa for consistency
     e_sat = a1 * np.exp(a3 * (T_dp - T_0) / (T_dp - a4))
     s_hum = R_dry / R_sat * e_sat / (p_air - e_sat * (1 - R_dry / R_sat))
-	# checking value type of wind
-    if np.isscalar(wind) == False:
-		raise Exception('Wind is not a scalar', print(wind)) 
     if wind == 0:
         Ri = 0
     else:
