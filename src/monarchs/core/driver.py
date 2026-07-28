be67bc85600a24720348833827b1929ffb73692b
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-07-28 09:36:34 +0100

    Update driver.py

--- src/monarchs/core/driver.py
+++ src/monarchs/core/driver.py
@@ -546,6 +546,11 @@ def initialise(model_setup):
     elif model_setup.met_data_source == "user_defined":
         setup_met_data.prescribed_met_data(model_setup)
 
+    if hasattr(model_setup, "RVf_input_filepath"):
+        print("monarchs.core.driver.initialise: RVf grid identified.")
+    else:
+        print("No RVf grid provided.")
+
     grid = initial_conditions.create_model_grid(
         model_setup.row_amount,
         model_setup.col_amount,
