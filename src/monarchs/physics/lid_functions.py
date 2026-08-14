70e2b793e5d0841218ee91c6e9274ab38566917f
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-08-14 14:11:28 +0100

    Update lid_functions.py sfc_flux() arg unpacking with alpha

--- src/monarchs/physics/lid_functions.py
+++ src/monarchs/physics/lid_functions.py
@@ -47,7 +47,7 @@ def virtual_lid(cell, dt, LW_in, SW_in, T_air, p_air, T_dp, T_rock, wind):
     original_mass = utils.calc_mass_sum(cell)
     x = np.array([cell["virtual_lid_temperature"]])
 
-    Q, Flat, Fsens = surface_fluxes.sfc_flux(
+    Q, Flat, Fsens, alpha = surface_fluxes.sfc_flux(
         cell["melt"],
         cell["exposed_water"],
         cell["lid"],
@@ -231,7 +231,7 @@ def lid_development(cell, dt, LW_in, SW_in, T_air, p_air, T_dp, T_rock, wind):
     """
     original_mass = utils.calc_mass_sum(cell)
     x = cell["lid_temperature"]
-    Q, Flat, Fsens = surface_fluxes.sfc_flux(
+    Q, Flat, Fsens, alpha = surface_fluxes.sfc_flux(
         cell["melt"],
         cell["exposed_water"],
         cell["lid"],
