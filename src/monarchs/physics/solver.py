343247e5a0f30286b2ac9c67c9d1050c0a6f98d7
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-09-23 14:55:45 +0100

    Debug/testing changes

--- src/monarchs/physics/solver.py
+++ src/monarchs/physics/solver.py
@@ -110,6 +110,10 @@ def firn_heateqn_solver(x, args, fixed_sfc=False, solver_method="hybr"):
             cell["Fsens"] = Fsens
             return soldict.x, soldict.success, soldict.message, soldict.success
 
+        residual_norm = np.linalg.norm(
+            heateqn.heateqn(soldict.x, cell, dt, dz, LW_in, SW_in, T_air, p_air, T_dp, wind)
+        ) # Remove after debugging
+
         sol = soldict.x
         ier = soldict.success
         mesg = soldict.message
